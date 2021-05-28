4c6476e8ba98b3dfd51ca4d52f60152354308439
spiknumb <ubuntu@ip-172-31-25-6.us-east-2.compute.internal>
2021-05-28 18:41:13 +0000

    added user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Bobert", password: "password")
+User.create(username: "Bobert1", password: "password1")
+User.create(username: "Bobert2", password: "password2")
+User.create(username: "Bobert3", password: "password3")
+User.create(username: "Bobert4", password: "password4")
+User.create(username: "Bobert5", password: "password5")
